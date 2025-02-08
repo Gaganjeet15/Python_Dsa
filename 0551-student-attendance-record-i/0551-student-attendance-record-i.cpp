8529d495f64cdebdebecd27df697666bc9e8c574
Gaganjeet Singh Digwa <130154312+Gaganjeet15@users.noreply.github.com>
2025-02-08 14:47:40 +0530

    Time: 0 ms (100%), Space: 8.3 MB (50.19%) - LeetHub

---
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool checkRecord(string s) {
+
+        int a =0;
+        int l =0;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == 'A'){
+                a+=1;                
+            }
+            if (i <= s.length() - 3 && s[i] == 'L' && s[i+1] ==     'L' && s[i+2] == 'L') {
+    return false;
+}
+        }
+        if(a>=2){
+            return false;
+        }
+        else {
+            return true;
+        }
+        return 0;
+    }
+};
